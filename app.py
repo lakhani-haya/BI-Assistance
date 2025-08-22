facda60e9d93fffd9b64e5ddf84451649eeb7394
Haya Lakhani <157073204+lakhani-haya@users.noreply.github.com>
2025-08-21 23:21:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Main entry
 import sys
 import os
 
-# Add src directory to path
+# Add here the src directory to path
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
 from src.dashboard import main
