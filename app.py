a1caf2c57fcd83458e11487a2a93cb3246f2ebf9
Haya Lakhani <157073204+lakhani-haya@users.noreply.github.com>
2025-11-03 23:33:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Streamlit App Entry Point
 import sys
 import os
 
-# Add here the src directory to path
+# Add here  src directory to path
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
 from src.dashboard import main
